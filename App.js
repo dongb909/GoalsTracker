905a52307c4cb8a592e956e6b8a48b19b0e35dec
bacai dong <dongb909@gmail.com>
2019-11-16 15:50:59 -0800

    add styles inline

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View , Button } from 'react-native';
+import {  Text, View , Button, TextInput } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutput] = useState('Open up App.js to start working')
+  // const [outputText, setOutputText] = useState('First text to appear on screen') //this is what state is first set as aka state: {} 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Button title="Our first button!" />
+    <View style={{padding:30}}> 
+
+      <View style={{flexDirection:'row', justifyContent:'space'}}>
+        <TextInput 
+          placeholder='Course Goal'
+          style={{borderColor:'f0f', borderWidth:1, margin:10}} 
+          />
+        <Button title="ADD" />
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   Wrapper = {
+//     padding:30
+//   }
+
+// });
